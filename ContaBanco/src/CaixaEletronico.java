014a5e1c2cbac0adfd6350e79d94f567a51e3ec4
Marcylene Barreto <marsouza93@gmail.com>
2024-05-07 15:27:31 -0300

    controle de candidato

--- ContaBanco/src/CaixaEletronico.java
+++ ContaBanco/src/CaixaEletronico.java
@@ -1,11 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 24.0;
-        double valorSolicitado = 25.0;
+        double valorSolicitado = 3.0;
 
-        if(valorSolicitado < saldo) 
+        if(valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
-        System.out.println(saldo);
-        
+            System.out.println(saldo);
+        } else {
+            System.out.println(saldo);
+        }
     }
 }
